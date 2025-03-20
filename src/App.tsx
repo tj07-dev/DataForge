afaff0d7f36b8c452368662518aa0c45b337e351
Tanmay Jain <tanmay.jain@nagarro.com>
2025-03-20 19:06:30 +0530

    refactor: remove unused CSS styles and reorganize imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import ErrorBoundary from './components/ErrorBoundary';
 import Layout from './components/Layout';
 import Home from './pages/Home';
